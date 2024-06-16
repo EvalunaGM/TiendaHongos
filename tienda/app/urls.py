1c95a8e954693c3d240e9f99606835ad27fc0c22
EvalunaGM <ev.gonzalezm@duocuc.cl>
2024-06-16 17:47:51 -0400

    a
    a

--- tienda/app/urls.py
+++ tienda/app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, contacto, somos, registro, shop
 
 urlpatterns = [
     path('', home, name="home"),
+    path('contacto/', contacto, name="contacto"),
+    path('registro/', registro, name="registro"),
+    path('somos/', somos, name="somos"),
+    path('shop/', shop, name="shop"),
 ]
--- tienda/app/views.py
+++ tienda/app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home (request):
     return render (request, 'app/home.html')
+
+def contacto (request):
+    return render (request, 'app/contacto.html')
+
+def shop (request):
+    return render (request, 'app/shop.html')
+
+def somos (request):
+    return render (request, 'app/somos.html')
+
+def registro (request):
+    return render (request, 'app/registro.html')
