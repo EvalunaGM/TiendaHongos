e020fe67c55780d4074169af8f994f576d174cce
EvalunaGM <ev.gonzalezm@duocuc.cl>
2024-06-22 21:02:52 -0400

    aa
    a

--- tienda/app/models.py
+++ tienda/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor (models.Model):
+    nombre= models.CharField(max_length=50)
+
+    def __str__(self):
+        return  self.nombre
+
+class Producto (models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.TextField(max_length=100)
+    imagen = models.ImageField(upload_to="productos", null=True)
+    proovedor = models.ForeignKey(Proveedor, on_delete=models.PROTECT) 
+
+    def __str__(self):
+        return  self.nombre 
--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+MEDIA_URL= 'media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
