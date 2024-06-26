183849371cd605f36e9ec2fc4c7b49e403925839
EvalunaGM <ev.gonzalezm@duocuc.cl>
2024-06-26 13:37:51 -0400

    a
    a

--- tienda/app/forms.py
+++ tienda/app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.forms import fields
 from .models import Contacto
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class ContactoForm(forms.ModelForm):
     class Meta:
         model = Contacto
         fields = ['nombre', 'correo', 'mensaje', 'tipo_consulta', 'avisos']
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username","email","password"]
